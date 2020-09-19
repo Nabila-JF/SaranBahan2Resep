7e1a1581c139637141920eba48bf9e4a7530c5e7
Nabila-JF <nabila060695@gmail.com>
2020-09-19 17:01:50 +0700

    Initial commit

---
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saranbahan2resep;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class SaranBahan2Resep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String identitas = "Nabila Jauza Firjatullah / XRPL1 / 27 ";
+        System.out.println("Identitas: "+identitas);
+        // \n = new line / garis baru
+        System.out.print("\nSaran Resep Dari Bahan Milik Anda\n");
+    }
+    
+}
